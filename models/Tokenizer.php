9217a6f750f89b111b8005172ba47d76bd377bf3
k0t9i <k0t9i@yandex.ru>
2017-03-22 16:05:23 +0500

    Refactor Tokenizer

--- models/Tokenizer.php
+++ models/Tokenizer.php
@@ -2,6 +2,13 @@
 
 namespace app\models;
 
+/**
+ * Class Tokenizer
+ *
+ * Tokenize input string
+ *
+ * @package app\models
+ */
 class Tokenizer
 {
     const TOKEN_NUMBER = '[0-9]+';
@@ -12,6 +19,11 @@ class Tokenizer
     const TOKEN_LEFT_PARENTHESIS = '\(';
     const TOKEN_RIGHT_PARENTHESIS = '\)';
 
+    /**
+     * List of existing tokens
+     *
+     * @var array
+     */
     private static $_tokens = [
         self::TOKEN_NUMBER,
         self::TOKEN_PLUS,
@@ -22,34 +34,77 @@ class Tokenizer
         self::TOKEN_RIGHT_PARENTHESIS
     ];
 
+    /**
+     * Parse input string into list of tokens
+     *
+     * @param string $string Input string
+     * @return array List of tokens from input string
+     */
     public function parse($string)
     {
         $input = $string;
         $result = [];
 
         $initLength = strlen($input);
-        if (!empty($input)) {
-            while (!empty($input)) {
-                if (substr($input, 0, 1) === ' ') {
-                    $input = substr($input, 1);
-                    continue;
-                }
-                $lengthBefore = strlen($input);
-                foreach (self::$_tokens as $token) {
-                    $token = '/^' . $token . '/';
-                    $matches = [];
-                    if (preg_match($token, $input, $matches)) {
-                        $result[] = $matches[0];
-                        $input = preg_replace($token, '', $input);
-                        break;
-                    }
-                }
-                if ($lengthBefore == strlen($input)) {
-                    throw new UnknownTokenException('Unknown token "' . $input[0] . '" at position ' . ($initLength - $lengthBefore + 1));
-                }
+        while (strlen($input) > 0) {
+            $token = $this->searchTokens($input, $initLength);
+            if ($token === false) {
+                break;
             }
+            $result[] = $token;
         }
 
         return $result;
+    }
+
+    /**
+     * Extracts first token from string and return it, or return false if token not found
+     * Changes input string
+     *
+     * @param string $string Input string
+     * @param string $tokenRegExp Token regular expression
+     * @return bool|mixed Token or false if token not dound
+     */
+    public function getFirstToken(&$string, $tokenRegExp)
+    {
+        $tokenRegExp = '/^' . $tokenRegExp . '/';
+        $matches = [];
+        if (preg_match($tokenRegExp, $string, $matches)) {
+            $string = preg_replace($tokenRegExp, '', $string);
+            return $matches[0];
+        }
+
+        return false;
+    }
+
+    /**
+     * Searches all tokens in input string and return first of it, or return false if token not found
+     * Changes input string
+     *
+     * @param string $string Input string
+     * @param int $initLength Initial length of string
+     * @return bool|mixed Token or false if token not found
+     * @throws UnknownTokenException if found unknown token
+     */
+    private function searchTokens(&$string, $initLength)
+    {
+        //Ignore leading spaces
+        $string = ltrim($string, ' ');
+        if (strlen($string) == 0) {
+            return false;
+        }
+
+        $lengthBefore = strlen($string);
+        foreach (self::$_tokens as $tokenRegExp) {
+            $token = $this->getFirstToken($string, $tokenRegExp);
+            if ($token !== false) {
+                return $token;
+            }
+        }
+        if ($lengthBefore == strlen($string)) {
+            throw new UnknownTokenException('Unknown token "' . $string[0] . '" at position ' . ($initLength - $lengthBefore + 1));
+        }
+        
+        return false;
     }
 }
